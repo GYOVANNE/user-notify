f128af1ca2b05cd721b02b7ccb9f28aaadddbcfe
Gyovanne <gyovanne@binome.com.br>
2023-08-17 17:15:01 -0300

    :hammer: remove unnecessary code and pass array in method send

--- app/Observers/InBoxUserObserver.php
+++ app/Observers/InBoxUserObserver.php
@@ -4,7 +4,6 @@ namespace App\Observers;
 
 use App\Models\InBoxUser;
 use App\Notifications\UserNotification;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Notification;
 
 class InBoxUserObserver
@@ -14,43 +13,6 @@ class InBoxUserObserver
      */
     public function created(InBoxUser $inBoxUser): void
     {
-        $user = $inBoxUser->user;
-        $message = [
-            'subject'=>$inBoxUser->subject,
-            'message'=>$inBoxUser->message
-        ];
-        Notification::send($user, new UserNotification($message));
-    }
-
-    /**
-     * Handle the InBoxUser "updated" event.
-     */
-    public function updated(InBoxUser $inBoxUser): void
-    {
-        //
-    }
-
-    /**
-     * Handle the InBoxUser "deleted" event.
-     */
-    public function deleted(InBoxUser $inBoxUser): void
-    {
-        //
-    }
-
-    /**
-     * Handle the InBoxUser "restored" event.
-     */
-    public function restored(InBoxUser $inBoxUser): void
-    {
-        //
-    }
-
-    /**
-     * Handle the InBoxUser "force deleted" event.
-     */
-    public function forceDeleted(InBoxUser $inBoxUser): void
-    {
-        //
+        Notification::send($inBoxUser->user, new UserNotification($inBoxUser->toArray()));
     }
 }
